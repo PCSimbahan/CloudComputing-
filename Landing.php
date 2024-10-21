e2bfdddc1977bea037602a375fcf677166d7a6f5
Patrick <113034310+PCSimbahan@users.noreply.github.com>
2024-10-21 11:43:57 +0800

    Add files via upload
    These are the files for the landing page alongside with the CSS file used for designing. I changed the name to style.css with small 's' to not overwrite the existing CSS file used in the registration page.

---
+++ Landing.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>  
+        <meta charset = "UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TheImperium40K</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+		<div class="header">	
+		<a href='#' class='clickable-home'>THE IMPERIUM 40K</a>
+		<nav class="indention"> 
+			<a href="#History.php">History</a>
+			<a href="#Tabletop.php">Tabletop</a>
+		</nav>
+		<a href='#about'class='clickable-word'>About</a>
+		</div>
+
+		
+		<div class="intro">
+		<h1>WELCOME TO THE IMPERIUM</h1>
+		</div>
+		<div class="format-paragraph">
+		<p> THE IMPERIUM OF MANKIND IS THE LAST HOPE THAT COULD SAVE THE UNIVERSE. NO KIND OF HERESY IS WELCOME TO THIS REAL. DO NOT DARE QUESTION THE DIVINITY AND POWER OF 
+			THE GREAT ALMIGHTY EMPEROR FOR IF YOU DO, YOU WILL BE BRANDED 'TILL DEATH. HERETICS ARE NOT TREATED KINDLY, NOR DO THEY GET AWAY WITH SUCH IDEAS OF REBELLION.</p>
+		</div>
+		<img src="Images/spacemarine.jpg" alt="space-marine" class="img1" width="920">
+		
+		<div class="intro2">
+		<h1>THE EMPEROR AWAITS...</h1>
+		</div>
+		<div class="format-paragraph2">	
+		<p> adaasadasdadawa asdasdasdaasd adasdadwdawdasdawdasdd adaw daw aww aw awdaw aw aw sd awd ada dda dad w a s w daw dawdwadadadadsdawaasdasda sdsd  asdasdadas dasdawasda sdw a adawadawdwadawdaddada dwadawdawdawd adwdasdkad hjbwhkdbaskhdbajsdbasj dbasjdbajdbajwbdjkasbdja sbdjkasbdasjkdbasjkdbaskdjbak jwbdkjasbdjkasbdasjkdbaskdjabkjawbkja
+		</p>
+		</div>	
+		<img src="Images/imperium.jpg" alt="Imperium-of-Men" class="img2" width="920">	
+
+	
+		<h1 id ='about' class='override-h1'>ABOUT THE IMPERIUM 40K</h1>
+		
+		<div class='about-group'>
+		<p>Welcome to TheImperium40k,
+		   this page exists for those who seek information and truth about the world history of Warhammer 40k from the conquest and domination of the Emperor of Mankind to the heresy 			 of Horus that led to the downfall of several Primarchs including Horus himself. All information in this page would support the start of your journey wherever it may be. This 		       website is to be enjoyed by all players of all status, from a complete Noob to Veteran players. A way to immerse oneself into the events that led to the current state of                     the Imperium and concurrent wars past.</p>
+		</div>
+	
+</body>
+</html>
+
